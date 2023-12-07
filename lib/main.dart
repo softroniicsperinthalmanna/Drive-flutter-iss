50346ffa2c7ffcf150626192aaecd9733e908d72
akshaykk07 <akshaykuruvangad@gmail.com>
2023-12-08 00:47:36 +0530

    second

--- lib/main.dart
+++ lib/main.dart
@@ -77,7 +77,7 @@ class MyApp extends StatelessWidget {
             // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
             // useMaterial3: true,
           ),
-          home: SkipdocumentScreen()),
+          home: StudentProfile()),
       designSize: Size(375, 812),
     );
   }
--- lib/screens/Students/StudentProfile.dart
+++ lib/screens/Students/StudentProfile.dart
@@ -14,18 +14,27 @@ class StudentProfile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 10).r,
+          child: backbutton(),
+        ),
+        title: Apptext3(
+            text: "Profile",
+            textcolor: btncolor,
+            size: 16.sp,
+            weight: FontWeight.w500),
+        centerTitle: true,
+        actions: [Image.asset("assets/icons/call.png")],
+      ),
       backgroundColor: backcolor,
       body: Stack(
         children: [
-          const SizedBox(
-            height: double.infinity,
-            width: double.infinity,
-          ),
           Image.asset("assets/images/vect.png"),
           Padding(
-            padding: const EdgeInsets.only(left: 20, right: 23, top: 50).r,
+            padding: const EdgeInsets.only(left: 20, right: 23, top: 100).r,
             child: Column(
-
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Stack(
@@ -34,17 +43,6 @@ class StudentProfile extends StatelessWidget {
                       child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                backbutton(),
-                                Text("Profile",
-                                    style: GoogleFonts.roboto(
-                                        fontWeight: FontWeight.w500,
-                                        fontSize: 16.sp)),
-                                Image.asset("assets/icons/call.png")
-                              ],
-                            ),
                             Padding(
                               padding: const EdgeInsets.only(top: 30).r,
                               child: Container(
@@ -55,8 +53,8 @@ class StudentProfile extends StatelessWidget {
                                     width: 150.w,
                                     child: CircleAvatar(
                                       radius: 60.r,
-                                      child:
-                                          Image.asset("assets/images/propic.png"),
+                                      child: Image.asset(
+                                          "assets/images/propic.png"),
                                     ),
                                   ),
                                   Expanded(
@@ -94,7 +92,7 @@ class StudentProfile extends StatelessWidget {
                             ),
                             Padding(
                               padding: const EdgeInsets.only(top: 20).r,
-                              child:  Apptext3(
+                              child: Apptext3(
                                   text: "Strong side:",
                                   textcolor: dark,
                                   size: 14.sp,
@@ -110,7 +108,7 @@ class StudentProfile extends StatelessWidget {
                           ]),
                     ),
                     Positioned(
-                        top: 120.r,
+                        top: 60.r,
                         left: 120.r,
                         child: FloatingActionButton(
                           onPressed: () {
@@ -118,59 +116,60 @@ class StudentProfile extends StatelessWidget {
                           },
                           mini: true,
                           backgroundColor: btncolor,
-                          child: Icon(Icons.edit,size: 18,),
                           elevation: 0,
                           shape: RoundedRectangleBorder(
                             side: BorderSide(color: backcolor, width: 4.w),
                             borderRadius: BorderRadius.circular(50).r,
                           ),
+                          child: const Icon(
+                            Icons.edit,
+                            size: 18,
+                          ),
                         ))
                   ],
                 ),
-                SizedBox(
-                  child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(top: 20).r,
-                          child: Row(
-                            children: [
-                              Apptext3(
-                                  text: "Your overall progress is ",
-                                  textcolor: btncolor,
-                                  size: 18.sp,
-                                  weight: FontWeight.w300),
-                              Apptext3(
-                                  text: "60%",
-                                  textcolor: btncolor,
-                                  size: 18.sp,
-                                  weight: FontWeight.w400)
-                            ],
-                          ),
+                Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 20).r,
+                        child: Row(
+                          children: [
+                            Apptext3(
+                                text: "Your overall progress is ",
+                                textcolor: btncolor,
+                                size: 18.sp,
+                                weight: FontWeight.w300),
+                            Apptext3(
+                                text: "60%",
+                                textcolor: btncolor,
+                                size: 18.sp,
+                                weight: FontWeight.w400)
+                          ],
                         ),
-                        Padding(
-                          padding:  EdgeInsets.only(top: 15).r,
-                          child:LinearPercentIndicator(
-                            lineHeight: 4,
-                            backgroundColor: Colors.grey,
-                            progressColor: btncolor,
-                            animation: true,
-                            animationDuration: 1000,
-                            percent: 0.5,
-                          ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 15).r,
+                        child: LinearPercentIndicator(
+                          lineHeight: 4,
+                          backgroundColor: Colors.grey,
+                          progressColor: btncolor,
+                          animation: true,
+                          animationDuration: 1000,
+                          percent: 0.5,
                         ),
-                        Padding(
-                          padding:  EdgeInsets.only(top: 20).r,
-                          child: Apptext3(
-                              text: "My Reports:",
-                              textcolor: btncolor,
-                              size: 24.sp,
-                              weight: FontWeight.w300),
-                        ),
-                      ]),
-                ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 20).r,
+                        child: Apptext3(
+                            text: "My Reports:",
+                            textcolor: btncolor,
+                            size: 24.sp,
+                            weight: FontWeight.w300),
+                      ),
+                    ]),
                 Padding(
-                  padding:  EdgeInsets.only(top: 20.h),
+                  padding: EdgeInsets.only(top: 20.h),
                   child: const Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
